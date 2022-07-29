f64877fbdd1734eeff3786d890052f2b7082925c
amesemyta1 <amesemyta@gmail.com>
2022-07-29 15:46:49 +0300

    first step

---
+++ lesson1/task2/index.js
@@ -0,0 +1,33 @@
+let sum = 0;
+
+for (let i = 0; i <= 1000; i += 1) {
+  if (i % 2 === 1) {
+    console.log('Found');
+    sum += i;
+    console.log(i, ' ', sum);
+
+    if (sum * 5 > 5000) {
+      console.log('Bigger');
+    } else {
+      console.log('Smaller or equal');
+    }
+    console.log(sum * 5);
+  }
+}
+
+let summ = 0;
+let d = 0;
+let m = 0;
+
+for (let i = 1; i <= 1000; i += 1) {
+  summ += i;
+}
+
+d = Math.round(summ / 1234);
+m = summ % 1234;
+
+console.log(summ);
+console.log(d);
+console.log(m);
+
+console.log(d > m);
