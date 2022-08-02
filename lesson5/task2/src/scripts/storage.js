69194742275e6454fab408e3b17a221a4cbc11c8
amesemyta1 <amesemyta@gmail.com>
2022-08-02 16:56:05 +0300

    ls5 tsk2

--- lesson5/task2/src/scripts/storage.js
+++
@@ -1,7 +0,0 @@
-const storage = {};
-
-export const setItem = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-export const getItem = key => JSON.parse(localStorage.getItem(key));
