5afa3b584bab6fae5a70d1db6c2f76131ac37118
amesemyta1 <amesemyta@gmail.com>
2022-08-03 15:01:05 +0300

    ls 7 tsk 1

---
+++ lesson7/task2/src/profile/index.js
@@ -0,0 +1,11 @@
+import { fetchUser } from './gateway';
+import printProfile from './printProfile';
+
+fetchUser('github').then(({ name, location }) =>
+  printProfile({
+    name,
+    location,
+  }),
+);
+
+  
